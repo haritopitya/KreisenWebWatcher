b54210265ca82db81e15d9f5e2aef51343310ede
haritopitya <tatu4321@icloud.com>
2022-03-27 03:04:59 +0900

    add 2D/3D button

--- src/components/home/map/TrackPlaneControl.js
+++ src/components/home/map/TrackPlaneControl.js
@@ -3,26 +3,43 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { useMap } from 'react-map-gl';
 
-const TrackPlaneButton = ({setIsTrack,setIsMoveFromFunc,position}) => {
+const TrackPlaneButton = React.memo(({ setIsTrack, setIsMoveFromFunc, position }) => {
     const map = useMap();
-    const onClick = () => {
+    const onClickTrack = () => {
         setIsTrack(true)
         setIsMoveFromFunc(true)
         map.current.flyTo({
-            center:[position.longitude,position.latitude]
+            center: [position.longitude, position.latitude]
         })
     }
-
+    const onClick3D = () => {
+        setIsMoveFromFunc(true)
+        map.current.flyTo({
+            pitch: 60
+        })
+    }
+    const onClick2D = () => {
+        setIsMoveFromFunc(true)
+        map.current.flyTo({
+            pitch: 0
+        })
+    }
     return (
         <div className='mapboxgl-control-container'>
             <div className='mapboxgl-ctrl-top-left'>
                 <div className='mapboxgl-ctrl mapboxgl-ctrl-group'>
-                    <button onClick={onClick}>
-                        <FontAwesomeIcon icon={faPlaneUp}/>
+                    <button onClick={onClickTrack}>
+                        <FontAwesomeIcon icon={faPlaneUp} />
+                    </button>
+                    <button onClick={onClick2D}>
+                        2D
+                    </button>
+                    <button onClick={onClick3D}>
+                        3D
                     </button>
                 </div>
             </div>
         </div>
     )
-}
+})
 export default TrackPlaneButton
