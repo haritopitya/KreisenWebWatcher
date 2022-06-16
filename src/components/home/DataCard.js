1644bf98601815d96bad8345991cdd72fd734162
haritopitya <tatu4321@icloud.com>
2022-06-17 02:44:32 +0900

    change datacard design

--- src/components/home/DataCard.js
+++ src/components/home/DataCard.js
@@ -1,37 +1,51 @@
 import React from 'react'
-import { Card, Col } from 'react-bootstrap'
-
+import { Card, Col, Row, Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { css } from '@emotion/react'
 const DataCard = ({ data }) => {
     if (!data) return null
     const styles = {
-        card: {
+        card: css({
             marginTop: '5px',
             marginBottom: '5px',
-        },
-        icon: {
-            fontSize: '50px',
-        },
-        val: {
+            borderColor:data.color
+        }),
+        icon: css({
+            fontSize: '40px',
+            color:data.color,
+        }),
+        val:css( {
             fontSize: '30px',
             color: data.color,
-        },
-        title: {
-            fontSize: '15px',
+        }),
+        title:css( {
+            fontSize: '12px',
             color: 'white',
-        },
+        }),
     }
     return (
         <Col>
-            < Card
+            <Card
                 bg='dark'
-                style={styles.card} >
+                css={styles.card} >
                 <Card.Body>
-                    <div style={styles.title}>
-                        {data.title}
-                    </div>
-                    <div style={styles.val}>
-                        {data.value}{data.unit}
-                    </div>
+                    <Container>
+                        <Row>
+                            <Col xs="4">
+                                <div css={styles.icon}>
+                                    <FontAwesomeIcon icon={data.icon} />
+                                </div>
+                            </Col>
+                            <Col xs="8">
+                                <div css={styles.title}>
+                                    {data.title}
+                                </div>
+                                <div css={styles.val} >
+                                    {data.value} {data.unit}
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Card.Body>
             </Card >
         </Col >
